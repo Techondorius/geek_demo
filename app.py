447c15b84b2c84a07bcae6cce8f21ae4c54e6186
Techondorius <kurofuku01@gmail.com>
2021-09-25 12:18:34 +0900

    環境変数をset_envで設定するように変更

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from project import create_app
 from flask import Flask
+from environment_variable import set_env
+import os
 
 app = create_app()
+kkk = set_env()     #環境変数をset_envで設定するように変更
+print(os.environ.get('FLASK_DEBUG'))
 
 if __name__ == '__main__':
     app.run()
